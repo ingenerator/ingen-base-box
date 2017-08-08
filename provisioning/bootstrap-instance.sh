049f13b8aeb1f120d6aff3c9affcae31e0299632
Andrew Coulton <andrew@ingenerator.com>
2017-08-08 15:47:56 +0100

    Update to chef 13.2.20

--- provisioning/bootstrap-instance.sh
+++ provisioning/bootstrap-instance.sh
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require_chef_version="12.18.31"
+require_chef_version="13.2.20"
 
 # ------------------------------------------------------------------------------
 # Error handlers
